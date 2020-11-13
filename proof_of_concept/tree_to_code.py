f6aeca62a5674e6c715f43ebccb4d4a3e62ef371
Tom Dymel <td@wps.de>
2020-11-13 20:14:20 +0100

    Adjusting the tree generation to print the actual predicted class instead

--- proof_of_concept/tree_to_code.py
+++ proof_of_concept/tree_to_code.py
@@ -14,7 +14,7 @@ def tree_to_code(tree, feature_names=range(0, 40)):
         for i in tree_.feature
     ]
 
-    file.write("unsigned char GestureClassificationDecisionTree(short int* features)")
+    file.write("unsigned char GestureClassificationDecisionTree(float* features)")
     file.write("{")
 
     def recurse(node, depth):
@@ -24,7 +24,7 @@ def tree_to_code(tree, feature_names=range(0, 40)):
             name = feature_name[node]
             threshold = tree_.threshold[node]
 
-            file.write("\n{}if (features[{}] <= {:0.0f})".format(indent, name, threshold) + " {")
+            file.write("\n{}if (features[{}] <= {:0.9f})".format(indent, name, threshold) + " {")
 
             recurse(tree_.children_left[node], depth + 1)
 
@@ -35,15 +35,17 @@ def tree_to_code(tree, feature_names=range(0, 40)):
             file.write("\n" + "{}".format(indent) + "}\n")
         else:
             classification_index = [ind for ind, x in enumerate(tree_.value[node][0]) if x != 0][0]
-            file.write("\n{}return {};\n".format(indent, classification_index))
+            file.write("\n{}return {};\n".format(indent, tree.classes_[classification_index]))
 
     recurse(0, 1)
 
     file.write("}\n")
     file.write("#include <stdio.h>\n")
     file.write("int main(int argc, char** argv) {\n")
-    file.write("short int args[27];\n")
-    file.write("for (char i = 0; i < 27; ++i) sscanf(argv[i+1], \"%hd\", &args[i]);\n")
+    # file.write("short int args[27];\n")
+    # file.write("for (char i = 0; i < 27; ++i) sscanf(argv[i+1], \"%hd\", &args[i]);\n")
+    file.write("float args[12];\n")
+    file.write("for (char i = 0; i < 12; ++i) sscanf(argv[i+1], \"%f\", &args[i]);\n")
     file.write("char result = GestureClassificationDecisionTree(args);\n")
     file.write("printf(\"Result: %d\\n\", (int) result);\n")
     file.write("}\n")
