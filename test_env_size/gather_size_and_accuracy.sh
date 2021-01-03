2a7dc6a3b34cefd4fbb02cddf88e795619b52ba4
Tom Dymel <td@wps.de>
2021-01-03 16:39:32 +0100

    First draft conclusion

--- test_env_size/gather_size_and_accuracy.sh
+++ test_env_size/gather_size_and_accuracy.sh
@@ -11,7 +11,7 @@ num_cores_per_node=$3
 env_num=$4
 
 echo "Working on: ${ccp_alpha},${min_leaf_sample}..."
-python3 ../../model/decision_tree.py 16 1 1 1 0 1 0.5 ${ccp_alpha} ${min_leaf_sample} 1 "../../" ${num_cores_per_node}
+python3 ../../model/decision_tree.py 16 1 1 1 0 1 0.5 ${ccp_alpha} ${min_leaf_sample} 1 "../../" ${num_cores_per_node} 0
 real_max_depth=$?
 ./../../../arduino-1.8.13/hardware/tools/avr/bin/avr-gcc -Os -std=gnu11 -mmcu=atmega328p -s -fwhole-program -fdata-sections -ffunction-sections -flto -fuse-linker-plugin -Wl,--gc-sections -Wl,--strip-all ./decision_tree.c -o decision_tree_avr
 size_in_bytes=$(wc -c ./decision_tree_avr | awk '{print $1}')
